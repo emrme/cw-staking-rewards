ccdc0209f4e3ef8dd4c85d905b14bcd371f64fef
‎ <107724647+username@users.noreply.github.com>
2022-11-18 10:41:22 +0300

    Add execute msg structure

--- src/contract.rs
+++ src/contract.rs
@@ -1,11 +1,13 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{to_binary, Addr, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cw2::set_contract_version;
 // use cw2::set_contract_version;
 
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 use crate::query::*;
+use crate::state::{Config, CONFIG, TOTAL_STAKED};
 /*
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:cw-yield-farming";
@@ -19,8 +21,25 @@ pub fn instantiate(
     _info: MessageInfo,
     _msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
-    // unimplemented!()
-    Ok(Response::new())
+    let validated_address = deps.api.addr_validate(&msg.owner)?;
+
+    let config = Config {
+        owner: validated_address,
+        staking_token: _msg.staking_token,
+        reward_token: _msg.reward_token,
+        reward_duration: _msg.reward_duration,
+        reward_rate: _msg.reward_rate,
+    };
+
+    set_contract_version(_deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    CONFIG.save(_deps.s, &config)?;
+
+    // TOTAL_STAKED.save(_deps.storage, &0u128)?; // TODO Check if its needed?
+
+    Ok(Response::new()
+        .add_attribute("method", "instantiate")
+        .add_attribute("sender", _info.sender))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -30,19 +49,23 @@ pub fn execute(
     _info: MessageInfo,
     _msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
+    // use ExecuteMsg::*;
+    match _msg {
+        ExecuteMsg::Receive(receive_msg) => {}
+        ExecuteMsg::Unstake { amount } => {}
+        ExecuteMsg::ClaimRewards {} => {}
+        ExecuteMsg::UpdateRewardRate { new_reward_rate } => {}
+    }
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
-    // unimplemented!()
-    use QueryMsg::*;
     match _msg {
-        StakerInfo { staker } => query_staker_info(_deps, staker),
-        StakingToken {} => query_staking_token(_deps),
-        RewardToken {} => query_reward_token(_deps),
-        TotalStaked {} => query_total_staked(_deps),
-        RewardRate {} => query_reward_rate(_deps),
+        QueryMsg::StakerInfo { staker } => query_staker_info(_deps, staker),
+        QueryMsg::StakingToken {} => query_staking_token(_deps),
+        QueryMsg::RewardToken {} => query_reward_token(_deps),
+        QueryMsg::TotalStaked {} => query_total_staked(_deps),
+        QueryMsg::RewardRate {} => query_reward_rate(_deps),
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod contract;
 mod error;
+pub mod execute;
 pub mod helpers;
 pub mod msg;
 pub mod query;
